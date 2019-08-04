0c0e43e315c84eeb7e797046f7f8b357ccd5a762
namsoo5 <are34@naver.com>
2019-08-04 16:07:14 +0900

    리펙토링 코드

--- prac_myFirstRxSwift/prac_myFirstRxSwift/ViewController.swift
+++ prac_myFirstRxSwift/prac_myFirstRxSwift/ViewController.swift
@@ -28,6 +28,40 @@ class ViewController: UIViewController {
     
     private func bindUI() {
         
+        //input  아이디 입력, 비밀번호 입력
+        let idInputOb = idTextField.rx.text.orEmpty.asObservable()
+        let idCheckOb = idInputOb.map(checkId)
+        
+        let pwInputOb = pwTextField.rx.text.orEmpty.asObservable()
+        let pwCheckOb = idInputOb.map(checkPw)
+        
+        //output 상태컬러, 로그인버튼 활성화
+        idCheckOb.subscribe(onNext: { b in
+            if b {
+                self.idColorView.backgroundColor = UIColor.blue
+            }else {
+                self.idColorView.backgroundColor = UIColor.red
+            }
+        })
+            .disposed(by: disposeBag)
+        
+        pwCheckOb.subscribe(onNext: { b in
+            if b {
+                self.pwColorView.backgroundColor = UIColor.blue
+            }else {
+                self.pwColorView.backgroundColor = UIColor.red
+            }
+        })
+            .disposed(by: disposeBag)
+        
+        Observable.combineLatest(idCheckOb, pwCheckOb, resultSelector: {$0 && $1})
+        .subscribe(onNext: {b in self.loginBtn.isEnabled = b})
+        .disposed(by: disposeBag)
+        
+        
+        
+        
+        /*
         idTextField.rx.text.orEmpty
             .map(checkId)   // 아이디 유형체크
             .subscribe(onNext: { s in   // s: true or false
@@ -41,7 +75,7 @@ class ViewController: UIViewController {
         
         
         pwTextField.rx.text.orEmpty
-            .map(checkPw)   // 아이디 유형체크
+            .map(checkPw)   // pw 유형체크
             .subscribe(onNext: { s in   // s: true or false
                 if s {
                     self.pwColorView.backgroundColor = UIColor.blue
@@ -50,6 +84,17 @@ class ViewController: UIViewController {
                 }
             })
             .disposed(by: disposeBag)
+        
+        Observable.combineLatest(
+            idTextField.rx.text.orEmpty.map(checkId), //아이디 유형을 체크한 값이 내려옴 true/false
+            pwTextField.rx.text.orEmpty.map(checkPw),
+            resultSelector: {s1, s2 in s1 && s2}
+            )
+            .subscribe(onNext: {s in  // s1 && s2 결과값
+                self.loginBtn.isEnabled = s
+            })
+            .disposed(by: disposeBag)
+        */
     }
 
     @IBAction func loginBtnClick(_ sender: Any) {
