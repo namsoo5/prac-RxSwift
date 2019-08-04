77345097d0323af52c4a2d55c644d52b2bd94892
namsoo5 <are34@naver.com>
2019-08-04 14:21:38 +0900

    storyboard구성

---
+++ prac_myFirstRxSwift/Pods/Target Support Files/Pods-prac_myFirstRxSwift/Pods-prac_myFirstRxSwift-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_prac_myFirstRxSwift : NSObject
+@end
+@implementation PodsDummy_Pods_prac_myFirstRxSwift
+@end
--- prac_myFirstRxSwift/prac_myFirstRxSwift/ViewController.swift
+++ prac_myFirstRxSwift/prac_myFirstRxSwift/ViewController.swift
@@ -10,11 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var idTextField: UITextField!
+    @IBOutlet weak var pwTextField: UITextField!
+    @IBOutlet weak var loginBtn: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func loginBtnClick(_ sender: Any) {
+    }
+    
 }
 
