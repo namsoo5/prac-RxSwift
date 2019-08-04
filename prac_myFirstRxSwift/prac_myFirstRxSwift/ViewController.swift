91d495f2586943d7a420b0ccf5524ebf1c546b52
namsoo5 <are34@naver.com>
2019-08-04 15:27:00 +0900

    id, pw입력창 rx적용

--- prac_myFirstRxSwift/prac_myFirstRxSwift/ViewController.swift
+++ prac_myFirstRxSwift/prac_myFirstRxSwift/ViewController.swift
@@ -7,19 +7,61 @@
 //
 
 import UIKit
+import RxCocoa
+import RxSwift
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var idTextField: UITextField!
+    @IBOutlet weak var pwColorView: UIView!
+    @IBOutlet weak var idColorView: UIView!
     @IBOutlet weak var pwTextField: UITextField!
     @IBOutlet weak var loginBtn: UIButton!
+    
+    var disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        bindUI()
+    }
+    
+    private func bindUI() {
+        
+        idTextField.rx.text.orEmpty
+            .map(checkId)   // 아이디 유형체크
+            .subscribe(onNext: { s in   // s: true or false
+                if s {
+                    self.idColorView.backgroundColor = UIColor.blue
+                }else {
+                    self.idColorView.backgroundColor = UIColor.red
+                }
+            })
+            .disposed(by: disposeBag)
+        
+        
+        pwTextField.rx.text.orEmpty
+            .map(checkPw)   // 아이디 유형체크
+            .subscribe(onNext: { s in   // s: true or false
+                if s {
+                    self.pwColorView.backgroundColor = UIColor.blue
+                }else {
+                    self.pwColorView.backgroundColor = UIColor.red
+                }
+            })
+            .disposed(by: disposeBag)
     }
 
     @IBAction func loginBtnClick(_ sender: Any) {
+        
     }
     
+    private func checkId(_ email: String) -> Bool {
+        return email.contains("@") && email.contains(".")
+    }
+    
+    private func checkPw(_ pw: String) -> Bool {
+        return pw.count > 5
+    }
 }
 
