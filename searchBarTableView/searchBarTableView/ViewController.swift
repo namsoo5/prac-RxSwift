7fdc79320e7ab70b8a4c3a28c059a4e6c6d9e239
namsoo5 <are34@naver.com>
2019-08-05 17:36:39 +0900

    tableview 구성

--- searchBarTableView/searchBarTableView/ViewController.swift
+++ searchBarTableView/searchBarTableView/ViewController.swift
@@ -12,11 +12,34 @@ import RxSwift
 
 class ViewController: UIViewController {
 
+    var items = [String]()
+    let samples = ["서울", "부산", "온수", "건대", "온수", "부천", "송파", "가", "가나", "가나다", "가나다라", "가카타파하", "에이"]
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
     }
 
+   
     
 }
 
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "item", for: indexPath)
+        cell.textLabel?.text = items[indexPath.row]
+        
+        return cell
+    }
+    
+    
+    
+    
+    
+    
+}
---
+++ searchBarTableView/searchBarTableView/searchTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  searchTableViewCell.swift
+//  searchBarTableView
+//
+//  Created by 남수김 on 05/08/2019.
+//  Copyright © 2019 ns. All rights reserved.
+//
+
+import UIKit
+
+class searchTableViewCell: UITableViewCell {
+    @IBOutlet weak var textLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
