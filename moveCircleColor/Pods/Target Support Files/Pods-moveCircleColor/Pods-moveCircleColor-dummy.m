cbc889e08a23983a301795861e98a814815dbb6c
namsoo5 <are34@naver.com>
2019-08-12 14:26:04 +0900

    원위치에 따른 색변경프로젝트시작

---
+++ moveCircleColor/Pods/Target Support Files/Pods-moveCircleColor/Pods-moveCircleColor-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_moveCircleColor : NSObject
+@end
+@implementation PodsDummy_Pods_moveCircleColor
+@end
---
+++ moveCircleColor/moveCircleColor/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  moveCircleColor
+//
+//  Created by 남수김 on 12/08/2019.
+//  Copyright © 2019 ns. All rights reserved.
+//
+import UIKit
+import RxSwift
+import RxCocoa
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
