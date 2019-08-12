041b5aef14c2a6d94f08a4c4d63ecb3374eb860a
namsoo5 <are34@naver.com>
2019-08-12 14:45:43 +0900

    원생성

--- moveCircleColor/moveCircleColor/ViewController.swift
+++ moveCircleColor/moveCircleColor/ViewController.swift
@@ -11,11 +11,30 @@ import RxCocoa
 
 class ViewController: UIViewController {
 
+    var circleView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setUp()
+    }
+    
+    func setUp() {
+        // 원 모양의 뷰를 그립니다
+        circleView = UIView(frame: CGRect(origin: view.center, size: CGSize(width: 100.0, height: 100.0)))
+        circleView.layer.cornerRadius = circleView.frame.width / 2.0
+        circleView.center = view.center
+        circleView.backgroundColor = .green
+        view.addSubview(circleView)
+        
+        let gestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(circleMove(_:)))
+        circleView.addGestureRecognizer(gestureRecognizer)
     }
 
+    @objc func circleMove(_ recognizer: UIPanGestureRecognizer){
+        let location = recognizer.location(in: view)
+            UIView.animate(withDuration: TimeInterval(0.1)) {
+            self.circleView.center = location
+        }
+    }
 
 }
 
